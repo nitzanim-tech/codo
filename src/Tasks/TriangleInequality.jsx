2a2069a7245f861eec843775e77541ee7596df85
hgbid <hagbid@gmail.com>
2023-11-08 17:53:26 +0200

    open beta to insts

--- src/Tasks/TriangleInequality.jsx
+++ src/Tasks/TriangleInequality.jsx
@@ -8,19 +8,27 @@ export function testsName() {
 export function getTaskExplanation() {
   return { generateExplanation: (selectedValue) => generateExplanation(selectedValue) };
 }
+
 export function generateExplanation(selectedValue) {
   return (
     <>
       {selectedValue.input && (
         <div dir="rtl">
           <p>
-            עבור המצב: <br />
-            גודל צלע- A {selectedValue.input.A + ','} <br />B {selectedValue.input.B + ','} צלע
-            <br />C{selectedValue.input.C} <br />
-            הפלט הנדרש אמור להכיל {selectedValue.ans} <br />
+            עבור הצלעות - {'a: ' + selectedValue.input.A + ', '}
+            {'b: ' + selectedValue.input.B + ', '}
+            {'c: ' + selectedValue.input.C + ' '}
+            <br />
+            {selectedValue.fullAns + ', '}
+            ולכן
+            {selectedValue.ans == 'cannot' && ' לא'} ניתן ליצור משולש.
+            <br />
+            כלומר, הפלט הנדרש אמור להכיל {selectedValue.ans}.
+            <br />
             ההדפסה האחרונה בקוד שכתבת {"('" + selectedValue.output + "')"}
+            {selectedValue.correct ? <p>מתאימה לפלט הנדרש. כל הכבוד!</p> : <p>לא מתאימה לפלט. נסו שוב :)</p>}
+            {/* <Triangle A={30} B={110} C={10} /> */}
           </p>
-          {selectedValue.correct ? <p>מתאימה לפלט הנדרש. כל הכבוד!</p> : <p>לא מתאימה לפלט. נסו שוב :)</p>}{' '}
         </div>
       )}
     </>
@@ -37,7 +45,8 @@ export function generateInputList() {
 
 export function processTestsOutputs(testsOutputs) {
   const names = testsName();
-  const answers = ['can ', 'cannot', 'cannot ', 'cannot ', 'can '];
+  const answers = ['can ', 'cannot', 'cannot', 'cannot', 'can '];
+  const fullAns = ['כל התנאים מתקיימים', '3+3 < 10', '3+1 < 11', '2+3 < 9', 'כל התנאים מתקיימים'];
   return testsOutputs.map((testsOutput, index) => {
     const inputLines = testsOutput.input.split('\n');
     const input = {
@@ -49,7 +58,7 @@ export function processTestsOutputs(testsOutputs) {
     const output = outputLines[outputLines.length - 2];
     const correct = output.includes(answers[index]);
     const name = names[index];
-    return { name, input, output, correct, ans: answers[index] };
+    return { name, input, output, correct, ans: answers[index], fullAns: fullAns[index] };
   });
 }
 
@@ -107,3 +116,15 @@ export function examples() {
     </>
   );
 }
+
+const Triangle = ({ A, B, C }) => {
+  const triangleStyle = {
+    width: 0,
+    height: 0,
+    borderLeft: `${A}px solid transparent`,
+    borderRight: `${B}px solid transparent`,
+    borderBottom: `${C}px solid #f00`, // Adjust the color as needed
+  };
+
+  return <div style={triangleStyle}></div>;
+};
--- src/components/NavBar/NavigateBar.jsx
+++ src/components/NavBar/NavigateBar.jsx
@@ -15,23 +15,28 @@ import RadioButtonUncheckedRoundedIcon from '@mui/icons-material/RadioButtonUnch
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const courseBookUrl = 'https://drive.google.com/file/d/19ZABUSmex80nO07J074tY0Ma2Cietb0p/view';
-const tasks = [
+const releasedTasks = [
   { value: 0, label: 'הכנה 0 - התנסות', finish: false },
   { value: 1, label: 'הכנה 1 - מעלית', finish: false },
-  { value: 2, label: 'תנאים 01 - אי שיוון המשולש', finish: false },
+];
+const betaTasks = releasedTasks.concat([
+  { value: 2, label: 'תנאים - אי שיוון המשולש', finish: false },
   { value: 3, label: 'תרגול מונחה - השערת קולץ', finish: false },
   { value: 4, label: 'תרגול מונחה - יאללה, למחזורית!', finish: false },
-];
+]);
+
 const selectItemDivide = [1, 2];
 
 export default function NavigateBar({ setTask, isShowTask }) {
   const [currentUser, setCurrentUser] = useState(null);
   const [studentData, setStudentData] = useState(null);
+  const [tasks, setTasks] = useState(releasedTasks);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       setCurrentUser(user);
       if (user) {
+        if (user.email.includes('@nitzanim.tech')) setTasks(betaTasks);
         const userData = await getCurrentUser({ app, id: user.uid });
         console.log(userData);
         setStudentData(userData);
