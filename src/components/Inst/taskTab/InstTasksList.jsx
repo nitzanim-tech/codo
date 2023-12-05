4abbd873d64e7b44c1033d877662615b59834791
hgbid <hagbid@gmail.com>
2023-12-05 09:06:26 +0200

    add status tab to inst page

--- src/components/Inst/taskTab/InstTasksList.jsx
+++ src/components/Inst/taskTab/InstTasksList.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from '@nextui-org/react';
 import { Directions } from '@mui/icons-material';
+import tasks from '../../../Tasks/TasksList.json';
 
 const DropdownWrapper = ({ children }) => (
   <div className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
     {children}
   </div>
 );
-
-const tasks = [
-  { key: 0, name: 'הכנה 0 - התנסות' },
-  { key: 1, name: 'הכנה 1 - מעלית' },
-  { key: 2, name: 'תנאים - אי שיוון המשולש' },
-  { key: 3, name: 'תרגול מונחה - השערת קולץ' },
-  { key: 4, name: '!תרגול מונחה - יאללה, למחזורית' },
-  { key: 5, name: 'wordle - רשימות' },
-  { key: 6, name: 'for -  מספר משוכלל' },
-  { key: 7, name: 'for -  שבע בום' },
-  { key: 8, name: 'for -  קרץ אותי קרץ' },
-];
 
 const dividers = [1, 2, 4, 5];
 
