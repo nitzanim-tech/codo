ede51b2ca00232a46ce3a9d50936eb37f389ee76
hgbid <hagbid@gmail.com>
2024-01-03 00:14:58 +0200

    fix multy args bug in python override print

--- src/components/IDE/RunCodeButton.jsx
+++ src/components/IDE/RunCodeButton.jsx
@@ -33,11 +33,12 @@ function RunCodeButton({ code, setOutput, setInputCallback, setError }) {
         },
       });
 
-      pyodide.runPython(`
-      import customPrint
-      def print(prompt=""):
-          return customPrint.print(prompt)
-      `);
+    pyodide.runPython(`
+    import customPrint
+    def print(*args, sep=" "):
+        prompt = sep.join(str(arg) for arg in args)
+        customPrint.print(prompt)
+    `);
 
       pyodide.runPython(`
       import customInput
