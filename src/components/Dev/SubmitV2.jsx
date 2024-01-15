3635ecc6fe1a475bdaa6da2c6275ce534b9dcacd
hgbid <hagbid@gmail.com>
2024-01-15 16:26:30 +0200

    improve DevTeam page (add a new task)

---
+++ src/components/Dev/SubmitV2.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import PythonIDE from '../components/IDE/PythonIDE';
+import NavBar from '../components/NavBar/NavigateBar';
+import Instructions from '../components/Instructions';
+import TestsList from '../components/TestsList/TestsList';
+import { Grid } from '@mui/material';
+import { PyodideProvider } from '../components/IDE/PyodideProvider';
+import { testsName } from '../Tasks/TaskIndex';
+import { getTaskByIndex } from '../components/IDE/getTaskByIndex';
+import { useFirebase } from '../util/FirebaseProvider';
+
+import './Submit.css';
+
+function Submit() {
+  const { userData } = useFirebase();
+  const { index } = useParams();
+  const [task, setTask] = useState(parseInt(index, 10) || 0);
+  const initialTestNames = testsName(task);
+  const [testsOutputs, setTestsOutputs] = useState(initialTestNames.map((name) => ({ name })));
+  const [taskObject, setTaskObject] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setTaskObject(getTaskByIndex({ index: task }));
+      const testNames = testsName(task);
+      const newEmptyTests = await Promise.all(testNames.map((name) => ({ name })));
+      setTestsOutputs(newEmptyTests);
+    };
+
+    fetchData();
+  }, [task]);
+
+  return (
+    <>
+      <NavBar task={task} setTask={setTask} isShowTask={true} />
+      <PyodideProvider>
+        <Grid container spacing={1} columns={3} rows={1} style={{ padding: '1.5%' }}>
+          <Grid item style={{ width: '20%' }}>
+            {((userData ? isReviewExist(userData.submissions, task) : false) ||
+              !taskObject?.hideTests ||
+              taskObject?.index == 15) && <TestsList testsOutputs={testsOutputs} task={task} />}
+          </Grid>
+
+          <Grid item style={{ width: '50%' }}>
+            <PythonIDE testsOutputs={testsOutputs} setTestsOutputs={setTestsOutputs} task={task} />
+          </Grid>
+
+          <Grid item style={{ width: '30%' }}>
+            {!taskObject?.hideTests && <Instructions task={task} />}
+          </Grid>
+        </Grid>
+      </PyodideProvider>
+    </>
+  );
+}
+
+export default Submit;
+
+const isReviewExist = (submissions, task) => {
+  if (!submissions || !submissions[task]) return false;
+  for (const trial of submissions[task].trials) if (trial.review) return true;
+  return false;
+};
--- src/pages/DevTeam.jsx
+++ src/pages/DevTeam.jsx
@@ -1,21 +1,31 @@
 import { useState } from 'react';
-import { AccordionItem, Accordion } from '@nextui-org/react';
-import SunEditor, { buttonList } from 'suneditor-react';
-import { Grid } from '@mui/material';
 
-import AcordionEditor from '../components/Dev/AcordionEditor';
+import AcordionTextEditor from '../components/Dev/AcordionTextEditor';
 import AcordionChip from '../components/Dev/AcordionChip';
 import Editor from '@monaco-editor/react';
+import AddTests from '../components/Dev/AddTests';
+import addTask from '../requests/tasks/addTask';
+import { useFirebase } from '../util/FirebaseProvider';
 
 const DevTeam = () => {
-const [code, setCode] = useState('write here')
+  const { app, userData } = useFirebase();
+  const [code, setCode] = useState('# write here');
+  const [chipsList, setChipsList] = useState([]);
+  const [testsList, setTestList] = useState([]);
+  const [description, setDescription] = useState('');
+  const [exampleText, setExampleText] = useState('');
+
+  const onSendClick = () => {
+    const newTask = { code, chipsList, description, exampleText, testsList, writer: userData.id };
+    addTask({ app, newTask });
+  };
 
   return (
-    <>
-      <AcordionChip title={'מה צריך לדעת'} />
-      <AcordionEditor title={'תיאור המשימה'} />
-      <AcordionEditor title={'דוגמה'} />
-      <p>קוד פתרון</p>
+    <div style={{ width: '80%', margin: '30px', justifyContent: 'center' }}>
+      <AcordionChip chipsList={chipsList} setChipsList={setChipsList} title={'מה צריך לדעת'} />
+      <AcordionTextEditor setText={setDescription} title={'תיאור המשימה'} />
+      <AcordionTextEditor setText={setExampleText} title={'דוגמה'} />
+      <h2>קוד פתרון</h2>
       <Editor
         height="315px"
         defaultLanguage="python"
@@ -23,11 +33,10 @@ const [code, setCode] = useState('write here')
         onChange={(newValue) => setCode(newValue)}
         options={{ minimap: { enabled: false } }}
       />
-      <p>טסטים</p>
-      <Button radius="full" isIconOnly variant="faded" onClick={handleAddClick}>
-        <ControlPointRoundedIcon />
-      </Button>
-    </>
+      <h2>טסטים</h2>
+      <AddTests testsList={testsList} setTestList={setTestList} />
+      <button onClick={onSendClick}>הדפסה</button>
+    </div>
   );
 };
 
