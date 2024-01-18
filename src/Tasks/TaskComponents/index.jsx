720d0f40713c703b70fc397b5956f4580f19bb3b
hgbid <hagbid@gmail.com>
2024-01-18 22:55:01 +0200

    add more tasks to db

--- src/Tasks/TaskComponents/index.jsx
+++ src/Tasks/TaskComponents/index.jsx
@@ -1,14 +1,16 @@
-import * as PerfectNumber from './PerfectNumber';
-import * as AliceElevator from './Preparation/AliceElevator';
+import * as HelloWord from './0Preparation/HelloWord';
+
+import * as AliceElevator from './0Preparation/AliceElevator';
+import * as PerfectNumber from './3for/PerfectNumber';
 
 const tasks = {
-  c3194b8af385: AliceElevator,
-  lri4kgr3s5k5p3943h: PerfectNumber,
+  '7e9e4f50c46c': HelloWord,
+  'c3194b8af385': AliceElevator,
+  'lri4kgr3s5k5p3943h': PerfectNumber,
 };
 
 // TestList.jsx
 export function getTaskExplanation({ task, selectedValue }) {
-  console.log(task);
   if (tasks.hasOwnProperty(task)) {
     return tasks[task].getTaskExplanation(selectedValue);
   } else {
