84ce20a91131e81a9664e8f2d1da34089e18b717
hgbid <hagbid@gmail.com>
2024-02-07 17:18:55 +0200

    inst can send review for each submittion

--- src/components/Inst/taskTab/VersionsButton.jsx
+++ src/components/Inst/taskTab/VersionsButton.jsx
@@ -3,8 +3,17 @@ import { Button, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from '@
 import HistoryRoundedIcon from '@mui/icons-material/HistoryRounded';
 import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
 import formatDate from '../../../util/formatDate';
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
 
-const VersionsButton = ({ versions, scoreSum }) => {
+const VersionsButton = ({ versions, scoreSum, student }) => {
+  const onVersionClick = (selectedVersion) => {
+    const versionToCheck = {
+      ...selectedVersion,
+      student,
+    };
+    localStorage.setItem('versionToCheck', JSON.stringify(versionToCheck));
+    window.open('/review', '_blank');
+  };
   return (
     <Dropdown aria-label="Versions menu">
       <DropdownTrigger>
@@ -15,8 +24,9 @@ const VersionsButton = ({ versions, scoreSum }) => {
       </DropdownTrigger>
       <DropdownMenu>
         {versions.map((version, index) => (
-          <DropdownItem key={index} textValue={`Version ${index + 1}`}>
+          <DropdownItem key={index} textValue={`Version ${index + 1}`} onClick={() => onVersionClick(version)}>
             <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              {version.review && <CheckCircleRoundedIcon sx={{ color: '#005395', marginRight: '15px' }} />}{' '}
               <span style={{ color: 'gray', marginRight: '10px' }}>{`${version.grade}/${scoreSum}`}</span>
               <span style={{ color: 'black' }}>{formatDate(version.date)}</span>
             </div>
