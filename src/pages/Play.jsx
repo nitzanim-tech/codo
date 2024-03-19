3e0dc961a025cb6f0402a3acf01f699b7d9a0beb
hgbid <hagbid@gmail.com>
2024-03-19 09:01:46 +0200

    add background to Play page

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,28 +1,26 @@
 import React, { useEffect, useState } from 'react';
-// import { useParams } from 'react-router-dom';
 
 import NavBar from '../components/NavBar/NavigateBar';
 import { Grid } from '@mui/material';
 import { PyodideProvider } from '../components/IDE/PyodideProvider';
-// import { useFirebase } from '../util/FirebaseProvider';
-// import getTaskById from '../requests/tasks/getTaskById';
-import './Submit.css';
+import './Play.css';
 import Board from '../Games/SET/Board';
 import Terminal from '../components/IDE/Terminal';
 import RunCodeButton from '../components/IDE/RunCodeButton';
+import woodBackground from '../assets/svg/wood.svg';
 
 function Play() {
-  const code=localStorage.getItem('code');
+  const code = localStorage.getItem('code');
   const [output, setOutput] = useState('');
   const [error, setError] = useState('');
   const [inputCallback, setInputCallback] = useState('');
-
   const [relevatOutput, setRelevatOutput] = useState(null);
   const [clickedCards, setClickedCards] = useState([]);
+  const [lastMassage, setLastMassage] = useState('');
 
   const handleInput = (value) => {
     if (inputCallback) {
-      // setInputValue('');
+      setLastMassage('');
       inputCallback(value);
       setInputCallback(null);
     }
@@ -32,51 +30,47 @@ function Play() {
     setRelevatOutput(getLastBoardFromOutput(output));
   }, [output]);
 
-    useEffect(() => {
-        handleInput(clickedCards[clickedCards.length - 1]);
-    }, [clickedCards]);
+  useEffect(() => {
+    handleInput(clickedCards[clickedCards.length - 1]);
+  }, [clickedCards]);
 
   const getLastBoardFromOutput = (output) => {
     const lines = output.split('\n');
     const boardLines = [];
-    for (let i = lines.length-1; i >= 0; i--) {
+    for (let i = lines.length - 1; i >= 0; i--) {
       if (['color', 'shape', 'shading', 'striped'].some((word) => lines[i].includes(word))) {
         boardLines.push(lines[i]);
-      } else if (boardLines.length != 0) break;
+      } else if (boardLines.length != 0) {
+        if (i > 0) setLastMassage(lines[i]);
+        break;
+      }
     }
-  return boardLines.reverse().join('\n') || '';
+    return boardLines.reverse().join('\n') || '';
   };
 
   return (
     <>
       <NavBar />
       <PyodideProvider>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <Grid container spacing={1} columns={3} rows={1} style={{ padding: '1.5%' }}>
-            <Grid item style={{ width: '50%' }}>
-              <h2>
-                <b>שלום</b>
-              </h2>
-              <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <Board boardText={relevatOutput} clickedCards={clickedCards} setClickedCards={setClickedCards} />
-              </div>
-            </Grid>
-            <Grid item style={{ width: '50%' }}>
-              <RunCodeButton
-                code={code}
-                setOutput={setOutput}
-                setError={setError}
-                setInputCallback={setInputCallback}
-              />
-              <Terminal
-                height={'480px'}
-                output={output}
-                error={error}
-                onInput={handleInput}
-                waitingForInput={!!inputCallback}
-              />
-            </Grid>
-          </Grid>
+        <div className="container">
+          <div className="left-half" style={{ backgroundImage: `url(${woodBackground})` }}>
+            <h2>
+              <b>{lastMassage}</b>
+            </h2>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <Board boardText={relevatOutput} clickedCards={clickedCards} setClickedCards={setClickedCards} />
+            </div>
+          </div>
+          <div className="right-half" style={{ backgroundColor: '#2b2b2b' }}>
+            <RunCodeButton code={code} setOutput={setOutput} setError={setError} setInputCallback={setInputCallback} />
+            <Terminal
+              height={'480px'}
+              output={output}
+              error={error}
+              onInput={handleInput}
+              waitingForInput={!!inputCallback}
+            />
+          </div>
         </div>
       </PyodideProvider>
     </>
