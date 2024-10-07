b3a1f2df34582a7f23735d5bdab99bcc21c37953
hgbid <hagbid@gmail.com>
2024-10-07 22:33:28 +0300

    center the TestsList div

--- src/components/TestsList/TestsList.jsx
+++ src/components/TestsList/TestsList.jsx
@@ -18,7 +18,6 @@ export default function TestsList({ testsOutputs, taskObject }) {
 
   return (
     <>
-      <h3>טסטים</h3>
       <ListboxWrapper>
         <Listbox aria-label="tests" onAction={handleSelect}>
           {testsOutputs.map((testsOutput, index) => (
@@ -49,11 +48,17 @@ export default function TestsList({ testsOutputs, taskObject }) {
   );
 }
 
-
 const ListboxWrapper = ({ children }) => (
   <div
-    className="w-full max-w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100"
-    style={{ background: 'white', fontSize: '140px' }}
+    className="w-full max-w-[320px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100"
+    style={{
+      background: 'white',
+      fontSize: '140px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: '0 auto', // Optional: for centering the wrapper within its parent container
+    }}
   >
     {children}
   </div>
