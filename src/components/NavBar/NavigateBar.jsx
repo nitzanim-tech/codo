1d508b5b7f98b911823b76d007da0c7f8713c24e
hgbid <hagbid@gmail.com>
2024-10-22 13:41:43 +0300

    update navbar color

--- src/components/NavBar/NavigateBar.jsx
+++ src/components/NavBar/NavigateBar.jsx
@@ -1,24 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react';
-import logoImg from '../../assets/img/logo.png';
-import codoLogoImg from '../../assets/img/codoLogo.png';
 
 import LoginOrRegisterDropdown from './LoginOrRegisterDropdown';
 import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 export default function NavigateBar() {
   return (
-    <Navbar dir="rtl" position="static" height={'40px'}>
-      <NavbarBrand style={{ flex: 1 }}>
-        <Link to="/">
-          <img src={codoLogoImg} style={{ width: '100px', marginRight: '-30%' }} />
-        </Link>
-        {/* <img src={logoImg} style={{ width: '100px' }} /> */}
-      </NavbarBrand>
+    <Navbar dir="rtl" position="static" height="50px" style={{ background: 'rgba(44, 36, 77, 0.9)' }}>
+      {/* <NavbarBrand style={{ flex: 1 }}>
+      </NavbarBrand> */}
 
       <NavbarContent justify="end" style={{ flex: 1 }}>
         <LoginOrRegisterDropdown />
+        <Link
+          to="/"
+          style={{
+            textDecoration: 'none',
+            color: 'white',
+            fontFamily: 'Bungee, sans-serif',
+            fontSize: '40px',
+            fontWeight: '400',
+          }}
+        >
+          CODO
+        </Link>
       </NavbarContent>
     </Navbar>
   );
+}
+
+// import logoImg from '../../assets/img/logo.png';
+// import codoLogoImg from '../../assets/img/codoLogo.png';
+{
+  /* <img src={codoLogoImg} style={{ width: '100px', marginRight: '-30%' }} /> */
+}
+{
+  /* <img src={logoImg} style={{ width: '100px' }} /> */
 }
--- src/components/Submit/Instructions.jsx
+++ src/components/Submit/Instructions.jsx
@@ -2,12 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { Chip, Divider } from '@nextui-org/react';
 import { Accordion, AccordionItem } from '@nextui-org/react';
 import SessionTracker from '../general/SessionTracker';
+import CustomScrollbar from '../general/CustomScrollbar';
 
 export default function Instructions({ taskObject }) {
+  console.log({ taskObject });
   return (
-    <div style={{ width: '100%', textAlign: 'right', direction: 'rtl' }}>
-      <Accordion dir="rtl" variant="splitted" selectionMode="multiple" isCompact>
-        <AccordionItem title="מה צריך לדעת?">
+    <CustomScrollbar>
+      <div style={{ height: '70vh', color: 'white', margin: ' 0 50px 0 50px' }}>
+        <div style={{ fontSize: `22px`, fontWeight: `500`, textAlign: 'right', width: '100%' }}>{taskObject.name}</div>
+        <hr className="solid" style={{ background: '#36356A', height: '1px', border: 'none' }} />
+
+        <div dangerouslySetInnerHTML={{ __html: taskObject.description }} />
+        <div style={{ width: '100%', textAlign: 'right', direction: 'rtl' }}>
+          <Accordion dir="rtl" variant="splitted" selectionMode="multiple" isCompact>
+            {/* <AccordionItem title="מה צריך לדעת?">
           <p style={{ marginBottom: '10px' }}>נושאים עיקריים: </p>
           <div style={{ display: 'flex', justifyContent: 'center', gap: '10px', flexWrap: 'wrap' }}>
             {taskObject.subjects &&
@@ -17,18 +25,24 @@ export default function Instructions({ taskObject }) {
                 </Chip>
               ))}
           </div>
-        </AccordionItem>
+        </AccordionItem> */}
 
-        <AccordionItem title="תיאור המשימה">
-          <div dangerouslySetInnerHTML={{ __html: taskObject.description }} />
-        </AccordionItem>
-        <AccordionItem title="דוגמה">
-          <div style={{ direction: 'ltr', textAlign: 'left' }}>
-            <div dangerouslySetInnerHTML={{ __html: taskObject.example }} />
-          </div>
-        </AccordionItem>
-      </Accordion>
-      <SessionTracker type={'copy'} />
-    </div>
+            <AccordionItem
+              title={<span style={{ color: 'white' }}>דוגמה</span>}
+              style={{
+                borderRadius: '10px',
+                background: '#463A6B',
+                boxShadow: '0px 0px 3px 0px rgba(255, 255, 255, 0.25)',
+              }}
+            >
+              <div style={{ direction: 'rtl', textAlign: 'left' }}>
+                <div dangerouslySetInnerHTML={{ __html: taskObject.example }} />
+              </div>
+            </AccordionItem>
+          </Accordion>
+          <SessionTracker type={'copy'} />
+        </div>
+      </div>
+    </CustomScrollbar>
   );
 }
